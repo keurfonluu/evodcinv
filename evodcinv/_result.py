3b2a37fa8548f73836eb9fb55ef540f308cf6f12
Keurfon Luu <keurfonluu@outlook.com>
2021-10-31 23:43:06 -0700

    implement concatenation of results

--- evodcinv/_result.py
+++ evodcinv/_result.py
@@ -31,6 +31,37 @@ class InversionResult(dict):
         """Return a list of attributes."""
         return list(self.keys())
 
+    def __add__(self, other):
+        assert isinstance(other, InversionResult)
+
+        if not self.keys():
+            return InversionResult(**other)
+
+        elif not other.keys():
+            return InversionResult(**self)
+
+        else:
+            misfit = min(self.misfit, other.misfit)
+            if misfit == self.misfit:
+                x = self.x
+                model = self.model
+
+            else:
+                x = other.x
+                model = other.model
+
+            return InversionResult(
+                misfit=misfit,
+                x=x,
+                model=model,
+                all_x=numpy.vstack([self.all_x, other.all_x]),
+                all_models=numpy.vstack([self.all_models, other.all_models]),
+                all_misfits=numpy.concatenate([self.all_misfits, other.all_misfits]),
+                global_misfits=numpy.row_stack((self.global_misfits, other.global_misfits)),
+                maxiter=([self.maxiter] if isinstance(self.maxiter, int) else self.maxiter) + ([other.maxiter] if isinstance(other.maxiter, int) else other.maxiter),
+                popsize=([self.popsize] if isinstance(self.popsize, int) else self.popsize) + ([other.popsize] if isinstance(other.popsize, int) else other.popsize),
+            )
+
     def write(self, filename, indent=None):
         from ._io import write
 
