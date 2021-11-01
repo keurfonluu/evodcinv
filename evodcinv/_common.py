7abece43373e4b8154b1a4bfc529c31f9848cab1
Keurfon Luu <keurfonluu@outlook.com>
2021-11-01 01:21:05 -0700

    add method plot_curve

--- evodcinv/_common.py
+++ evodcinv/_common.py
@@ -2,3 +2,9 @@ itype = {
     "phase": 0,
     "group": 1,
 }
+
+
+units = {
+    "frequency": "Hz",
+    "period": "s",
+}
--- evodcinv/_result.py
+++ evodcinv/_result.py
@@ -1,6 +1,14 @@
 import numpy
 
 import matplotlib.pyplot as plt
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
+from matplotlib.ticker import ScalarFormatter
+
+from disba import surf96
+from disba._common import ifunc
+
+from ._common import itype, units
 
 
 class InversionResult(dict):
@@ -49,6 +57,110 @@ class InversionResult(dict):
                 maxiter=([self.maxiter] if isinstance(self.maxiter, int) else self.maxiter) + ([other.maxiter] if isinstance(other.maxiter, int) else other.maxiter),
                 popsize=([self.popsize] if isinstance(self.popsize, int) else self.popsize) + ([other.popsize] if isinstance(other.popsize, int) else other.popsize),
             )
+
+    def plot_curve(
+        self,
+        t,
+        mode,
+        wave,
+        type,
+        all=False,
+        perc=0.99,
+        disba_args=None,
+        plot_args=None,
+        ax=None,
+    ):
+        # disba arguments
+        disba_args = disba_args if disba_args is not None else {}
+        _disba_args = {
+            "algorithm": "dunkin",
+            "dc": 0.001,
+            "dt": 0.025,
+        }
+        _disba_args.update(disba_args)
+
+        algorithm = _disba_args.pop("algorithm")
+        dc = _disba_args.pop("dc")
+        dt = _disba_args.pop("dt")
+
+        def getc(thickness, velocity_p, velocity_s, density):
+            c = surf96(
+                t,
+                thickness,
+                velocity_p,
+                velocity_s,
+                density,
+                mode,
+                itype[type],
+                ifunc[algorithm][wave],
+                dc,
+                dt,
+            )
+            idx = c > 0.0
+            
+            return c[idx]
+
+        # Plot arguments
+        plot_args = plot_args if plot_args is not None else {}
+        _plot_args = {
+            "type": "line",
+            "xaxis": "period",
+            "cmap": "viridis_r",
+        }
+        _plot_args.update(plot_args)
+
+        plot_type = _plot_args.pop("type")
+        xaxis = _plot_args.pop("xaxis")
+        cmap = _plot_args.pop("cmap")
+
+        assert plot_type in {"line", "loglog", "semilogx", "semilogy"}
+        assert xaxis in {"frequency", "period"}
+
+        plot_type = plot_type if plot_type != "line" else "plot"
+        plot = getattr(plt if ax is None else ax, plot_type)
+        x = 1.0 / t if xaxis == "frequency" else t
+
+        if all:
+            # Filter and sort models
+            models, misfits = self._filter_results(perc)
+            idx = numpy.argsort(misfits)[::-1]
+            models = models[idx]
+            misfits = misfits[idx]
+
+            # Make colormap
+            norm = Normalize(misfits.min(), misfits.max())
+            smap = ScalarMappable(norm, cmap)
+            smap.set_array([])
+
+            # Generate and plot curves
+            curves = [getc(*model.T) for model in models]
+            for curve, misfit in zip(curves, misfits):
+                plot(x, curve, color=smap.to_rgba(misfit), **_plot_args)
+
+        else:
+            c = getc(*self.model.T)
+            plot(x, c, **_plot_args)
+
+        # Customize axes
+        gca = ax if ax is not None else plt.gca()
+
+        xlabel = f"{xaxis.capitalize()} [{units[xaxis]}]"
+        ylabel = f"{type.capitalize()} velocity [km/s]"
+        gca.set_xlabel(xlabel)
+        gca.set_ylabel(ylabel)
+
+        gca.set_xlim(x.min(), x.max())
+
+        # Disable exponential tick labels
+        gca.xaxis.set_major_formatter(ScalarFormatter())
+        gca.xaxis.set_minor_formatter(ScalarFormatter())
+
+    def _filter_results(self, perc=0.99):
+        apost = numpy.exp(-0.5 * self.misfits ** 2)
+        threshold = perc * apost.max()
+        idx = apost > threshold
+        
+        return self.models[idx], self.misfits[idx]
 
     def write(self, filename, indent=None):
         from ._io import write
