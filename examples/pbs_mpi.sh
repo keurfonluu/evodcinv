49bafcbf088c1f8b8a25180c66e5ca6ffefdc548
Keurfon Luu <keurfonluu@outlook.com>
2021-12-12 16:08:45 -0800

    remove examples

--- examples/pbs_mpi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# MPI Parameters
-MPIEXEC="/pp1/sw_tools/openmpi_4_0_2_for_pbspro/bin/mpiexec" # --tag-output" ##Option specifies which node is running
-EXEC="python example_rayleigh_dcinv.py"
-
-# Command
-$MPIEXEC --hostfile $PBS_NODEFILE $EXEC
